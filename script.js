0471293114a7bc05859e9c69f20ffbd5f0b0d0b1
Anthony Smyth <Anthony.smythh@gmail.com>
2024-03-18 15:20:05 +0000

    function and design bones

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const messageGenerator = {
+    arr1: ['not going', 'definately going', 'unlikely', 'highly likely'],
+    arr2: ['die', 'be rich', 'age well', 'be healthy'],
+    arr3: ['be powerful', 'peaceful', 'live long']
+}
+
+//console.log(messageGenerator.arr1[Math.floor(Math.random()* messageGenerator.arr1.length)])//
+
+const message = () => {
+ return `You are ${messageGenerator.arr1[Math.floor(Math.random()* messageGenerator.arr1.length)]} to ${messageGenerator.arr2[Math.floor(Math.random()* messageGenerator.arr2.length)]} and ${messageGenerator.arr3[Math.floor(Math.random()* messageGenerator.arr3.length)]}.`
+}
+console.log(message())
